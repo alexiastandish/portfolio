e022ec3e83cdd2b175834d5bb68c91945ea54de3
alexiastandish <alexiastandish@gmail.com>
2018-12-05 12:10:20 -0600

    docs

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-  app = express()
-
-const path = require('path')
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
