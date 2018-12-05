7fc11fcde108aa043e5395407baa3a384b4e9e70
alexiastandish <alexiastandish@gmail.com>
2018-12-05 11:39:16 -0600

    build

---
+++ build/precache-manifest.0373d097b70a898731786e3dabd5498f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0923d079acd47bc3eda6",
+    "url": "/static/css/main.504f6af1.chunk.css"
+  },
+  {
+    "revision": "0923d079acd47bc3eda6",
+    "url": "/static/js/main.0923d079.chunk.js"
+  },
+  {
+    "revision": "18b5a908b5b6f9768b30",
+    "url": "/static/js/1.18b5a908.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dd77d7e6201412d304121cf4be005f9",
+    "url": "/index.html"
+  }
+];
